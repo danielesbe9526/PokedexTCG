ef54de502d15b5ffb67ab40423122c1a58e93be1
Daniel Beltran <daniel.beltran@cornershopapp.com>
2023-04-11 14:33:12 -0500

    Initial Commit

---
+++ PokedexPTC/PokedexPTCApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexPTCApp.swift
+//  PokedexPTC
+//
+//  Created by Daniel Beltran on 11/04/23.
+//
+
+import SwiftUI
+
+@main
+struct PokedexPTCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
